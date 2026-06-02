6427c1b138a21dd135a3a870ace52997de7d1b31
ViChagas07 <alissonchagas986@gmail.com>
2026-06-02 18:45:39 -0300

    .

--- Back-end/src/pytomatiza/infrastructure/db/session.py
+++ Back-end/src/pytomatiza/infrastructure/db/session.py
@@ -16,6 +16,9 @@ engine = create_async_engine(
     settings.DATABASE_URL,
     echo=False,
     pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+    pool_recycle=3600,
     connect_args={
         "statement_cache_size": 0,
     }
