8a6b9207849a3ced435271144bc43e1d300799c3
ViChagas07 <alissonchagas986@gmail.com>
2026-06-03 19:26:45 -0300

    yup

--- Back-end/src/pytomatiza/infrastructure/db/session.py
+++ Back-end/src/pytomatiza/infrastructure/db/session.py
@@ -1,21 +1,19 @@
-"""Async session factory."""
-
 from typing import AsyncGenerator
-
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
 
-
-
 from pytomatiza.config import settings
-
 
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=settings.DB_ECHO,
+
+    connect_args={
+        "statement_cache_size": 0
+    }
 )
 
 AsyncSessionLocal = async_sessionmaker(
@@ -23,7 +21,6 @@ AsyncSessionLocal = async_sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False,
 )
-
 
 async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
